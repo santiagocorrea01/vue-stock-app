92bb06b805ca6ef9b0407173397b13697b1e09fa
Santiago Correa <santiagocorrea@Santiagos-MacBook-Air.local>
2020-12-30 13:15:56 -0500

    Add dist for netlify

---
+++ dist/precache-manifest.fb396e5f1d0199f7df1256e982adc3b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c517d2a7aa1e03762425",
+    "url": "/hello-world/css/app.c103fcc0.css"
+  },
+  {
+    "revision": "e719bc63b47d9680c3a3",
+    "url": "/hello-world/css/chunk-vendors.9907f2c2.css"
+  },
+  {
+    "revision": "7305e388f2125e3b995b1bbd83afb5b0",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "0c64ba0da4600d91b904",
+    "url": "/hello-world/js/about.2c7080ba.js"
+  },
+  {
+    "revision": "c517d2a7aa1e03762425",
+    "url": "/hello-world/js/app.ab19719d.js"
+  },
+  {
+    "revision": "e719bc63b47d9680c3a3",
+    "url": "/hello-world/js/chunk-vendors.152353d0.js"
+  },
+  {
+    "revision": "65134e2a7a2a5445d7381b7b77c97356",
+    "url": "/hello-world/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello-world/robots.txt"
+  }
+]);
